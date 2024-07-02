5b5f630301cc1bfb17b2318a365c649fa3f70b6b
SK IRFAN <141660427+SKIRFAN5748@users.noreply.github.com>
2024-07-02 23:21:42 +0530

    Delete ebanking system directory

--- ebanking system/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("connect.php");
-$sql = mysqli_query($conn,"DELETE FROM users where users_id = '$_GET[id]'");
-$sql1 = mysqli_query($conn,"DELETE FROM account_details where users_id = '$_GET[id]'");
-header('Location:adminhome.php');
-?>
